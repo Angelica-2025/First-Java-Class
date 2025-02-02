6a01ad3848db783267c0b258cf8d6037e7d36d7e
Rosa <angelica-2025@outlook.es>
2025-02-02 23:32:21 +0100

    Add files via upload

---
+++ primera_java_class/src/main/java/first_java_class/First_Java_Class.java
@@ -0,0 +1,5 @@
+package first_java_class;
+
+public class First_Java_Class {
+
+}
---
+++ primera_java_class/src/test/java/dev/rosa/primera_java_class/AppTest.java
@@ -0,0 +1,23 @@
+package dev.rosa.primera_java_class;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void testEquals() {
+        assertEquals(2, 2);
+    }
+}
